82f66879656249040960b107487fab7491a50583
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-03-05 21:35:21 +0900

    Initial Commit

---
+++ githubApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubApi
+//
+//  Created by 福山帆士 on 2020/03/05.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
